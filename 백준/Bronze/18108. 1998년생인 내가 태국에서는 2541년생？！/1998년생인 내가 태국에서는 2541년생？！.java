bb6dd0afa5f47414fa26c9dc245a01849b71cfdb
jayoung <96919844+kjy222@users.noreply.github.com>
2022-06-15 12:42:51 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 224 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,15 @@
+// 서기-예수 그리스도 태어난 해
+// 불기-석가모니가 입멸한 해
+// 불기=서기+544
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        
+        int year=sc.nextInt();
+        
+        System.out.print(year-543);
+    }
+}
